de6f0694ddfe39c82ad3a18da900939a12ba70d7
Manue Augereau <manuelleaugereau@gmail.com>
2025-11-13 15:21:29 +0100

    WIP

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,26 +1,28 @@
-// app/javascript/controllers/search_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["input", "form", "results"]
-
-  connect() {
-    this.timeout = null
-  }
+  static targets = ["form", "input", "results"]
 
   perform() {
     clearTimeout(this.timeout)
-
-    const query = this.inputTarget.value
-
-    if (query.length < 2) {
-      this.resultsTarget.innerHTML = ""
-      return
-    }
-
-    // Debounce de 300ms
     this.timeout = setTimeout(() => {
-      this.formTarget.requestSubmit()
+      const query = this.inputTarget.value
+
+      if (query.length > 2) {
+        const url = new URL(this.formTarget.action)
+        url.searchParams.set('query', query)
+
+        fetch(url, {
+          headers: {
+            'Accept': 'text/vnd.turbo-stream.html',
+            'Turbo-Frame': 'search_results'
+          }
+        })
+        .then(response => response.text())
+        .then(html => Turbo.renderStreamMessage(html))
+      } else {
+        this.resultsTarget.innerHTML = ''
+      }
     }, 300)
   }
 }
